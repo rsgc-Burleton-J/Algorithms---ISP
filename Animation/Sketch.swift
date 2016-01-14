624e1edd283a4249031a65862e9647d9ec0d3eee
rsgc-Burleton-J <jburleton@rsgc.on.ca>
2016-01-14 15:28:08 -0500

    Empty IPO

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Darwin
 
 class Sketch {
     
@@ -19,6 +20,9 @@ class Sketch {
     var s = 10
     var y : Double = 0
     var colour : Float = 0
+    var D = 5
+    var Size = 0
+    var counter = 0
     
     
     // This runs once, equivalent to setup() in Processing
@@ -51,25 +55,102 @@ class Sketch {
     func draw() {
         
         z = z + 1
-        colour = colour + 0.3
+        colour = colour + 0.35
+        var radian = (2*M_PI*z)/360
         
         // Horizontal position of circle
         //x = x + s
         x = Double(canvas.frameCount)
    
-        y = (sin (z)) * 400
+        //y = (sin(z))
         
-//        y = 0.05*(x-200)*(x-200)+100
+        // convert degrees (z) into radians
+        //iplement formula
+
+        if (D == 1) {
+        Size = 5
+        
+        y = (sin(radian)) * 200 + 250
+        print(y)
+    
+            if (x > 1000){
+            x = 0
+                counter = counter + 1
+            }
+            if (counter > 0){
+                x = z - (1000)
+            
+                y = (sin(radian)) * 100 + 250
+            }
+        }
+        
+        if (D == 2){
+            Size = 5
+            y = ((1/4)*sin(radian)) * 200 + 250
+            print(x)
+            
+            if (x > 1000){
+                x = 0
+                counter = counter + 1
+            }
+            if (counter > 0){
+                x = z - (1000)
+                
+                y = ((1/2)*sin(radian)) * 200 + 250
+            }
+        }
+        
+        if (D == 3){
+            Size = 5
+            y = (sin(z)) * 400
+            
+            if (x > 1000){
+                x = 0
+                counter = counter + 1
+            }
+            if (counter > 0){
+                x = z - (1000)
+                
+                y = ((1/2)*sin(z)) * 400
+            }
+
+       }
+        if (D == 4){
+            Size = 5
+            y = (tan(z)) * 400
+            
+            if (x > 1000){
+                x = 0
+                counter = counter + 1
+            }
+            if (counter > 0){
+                x = z - (1000)
+                
+                y = ((1/2)*tan(z)) * 400
+            }
+            
+        }
+        if (D == 5) {
+            Size = 20
+            y = 0.1*(x-200)*(x-200)+200
+            if (y < 300) {
+            y = (sin (z)) * 10
+            y = -0.05*(x-200)*(x-200)+200
+        }
+            if (y > 400) {
+            y = 0.01*(x-200)*(x-200)
+        }
+        }
         
         
         // Bounce when hitting wall
-        if (x > Double(canvas.width) || x < 0) {
-            s *= -1
-        }
-//        
+//        if (x > Double(canvas.width) || x < 0) {
+//            s *= -1
+//        }
+//
 //        if (y < 300) {
 //           y = (sin (z)) * 10
-        // y = -0.05*(x-200)*(x-200)+200
+// y = -0.05*(x-200)*(x-200)+200
 //        }
 //        if (y > 400) {
 //            y = 0.01*(x-200)*(x-200)
@@ -85,9 +166,11 @@ class Sketch {
         // Draw a circle that moves across the screen
         canvas.drawShapesWithBorders = false
         canvas.fillColor = Color(hue: colour, saturation: 100, brightness: 100, alpha: 100)
-        canvas.drawEllipse (centreX: Int(x), centreY: Int(y), width: 30, height: 30)
+        canvas.drawEllipse (centreX: Int(x), centreY: Int(y), width:Size, height: Size)
+        
+        canvas.textColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100) //Black
+        canvas.drawText(message: String(z))
         
         
     }
-    
 }
